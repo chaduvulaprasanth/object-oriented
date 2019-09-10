b026b55ac3171d234f7de0183ff3f1e1c32a0b91
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-09-10 21:57:17 +0530

    adding data and functions in object in factoryway

--- main.js
+++ main.js
@@ -89,3 +89,36 @@ prasanth.decrement()
 prasanth.decrement()
 prasanth
 // output is {name: "prasanth", score: 6}
+
+
+
+// given task data: name,blanace,username and function: changeName,incrementBalance, decrementBalance, changeUsername
+
+// factoryway of writing code
+function userAccount(name,username,balance = 0){
+    var accountDetails = {};
+    accountDetails.name = name;
+    accountDetails.username = username;
+    accountDetails.balance = balance;
+    accountDetails.changename = (changename) => {return accountDetails.name = changename};
+    accountDetails.incrementBalance = () => {return accountDetails.balance++};
+    accountDetails.decrementBalance = () => {return accountDetails.balance--};
+    accountDetails.changeusername = (changeusername) => {return accountDetails.username = changeusername};
+    return accountDetails;
+}
+var sam = userAccount("sam","radha")
+sam
+// output is {name: "sam", username: "radha", balance: 0, changename: ƒ, incrementBalance: ƒ, …}
+sam.changename("krishna")
+sam
+// output is {name: "krishna", username: "radha", balance: 3, changename: ƒ, incrementBalance: ƒ, …}
+sam.incrementBalance()
+sam.incrementBalance()
+sam.incrementBalance()
+sam.incrementBalance()
+sam.decrementBalance()
+sam
+// output is {name: "sam", username: "radha", balance: 3, changename: ƒ, incrementBalance: ƒ, …}
+sam.changeusername("mukunda")
+sam
+// output is {name: "krishna", username: "mukunda", balance: 3, changename: ƒ, incrementBalance: ƒ, …}
